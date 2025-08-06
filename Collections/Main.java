c69f95bec565f205f0aac0b60c6349595152fb46
Naveenx03 <krishnanaveen2003@gmail.com>
2025-08-06 11:49:57 +0530

    modified

--- Collections/Main.java
+++ Collections/Main.java
@@ -1,13 +1,26 @@
 class Main{
     public static void main(String[] args) {
-        Stack stack = new Stack(5);
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        stack.display();
-        System.out.println();
-        System.out.println(stack.peek());
+        Stack ins1 = new Stack(5);
+        Stack ins2= new Stack(5);
+        Stack output = new Stack(5);
+        ins1.push(6);
+        ins1.push(8);
+        ins1.push(9);
+        ins1.push(11);
+        ins1.push(4);        
+
+        ins2.push(1);
+        ins2.push(2);
+        ins2.push(3);
+        ins2.push(4);
+        ins2.push(5);
+        
+        while(!ins1.isEmpty() && !ins2.isEmpty()){
+            int a = ins1.pop();
+            int b = ins2.pop();
+            output.push(a+b);
+        }
+
+        output.display();
     }
 }
